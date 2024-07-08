674e291933b60d69572113f075ad1d440adeba08
Elaoud, Mohamed Amin <Mohamed-Amin.Elaoud@adesso.de>
2024-07-08 11:07:01 +0200

    github setup

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { onUpdated, ref } from 'vue'
+const formValues = ref({});
+function setContent(){
+  const content = document.querySelector("#widget-content").innerHTML;
+  console.log(content);
+}
+
+const vueform = ref({
+
+  size: 'md',
+  displayErrors: false,
+  onChange: (Values) => { formValues.value = Values },
+  schema: {
+    title: {
+      type: 'static',
+      content: 'Custom Widget',
+      tag: 'h1'
+    },
+
+    headline: {
+      type: 'text',
+    },
+
+    content: {
+      type: 'editor',
+    }
+
+  }
+
+})
+
+onUpdated(setContent);
+
+
+</script>
+
+
+<template>
+  <Vueform v-bind="vueform" />
+  <div id="widget-content">
+    <h1> {{ formValues.headline }}</h1>
+    <div v-html="formValues.content"></div>
+  </div>
+
+
+</template>

54ac7bea1310fb955cb7d5c3aade0b2d956bd139
Elaoud, Mohamed Amin <Mohamed-Amin.Elaoud@adesso.de>
2024-07-09 14:07:07 +0200

    component layout

--- src/components/CustomeElement.vue
+++ src/components/CustomeElement.vue
@@ -1,54 +1,57 @@
 <!-- CustomElement.vue -->
 
+<script>
+import { ref } from 'vue'
+import { defineElement } from '@vueform/vueform'
+import Explorer from './explorer/Explorer.vue';
 
-  
-  <script>
-    import { ref } from 'vue'
-    import { defineElement } from '@vueform/vueform'
-  
-    export default defineElement({
-      name: 'CustomElement',
-      setup(props, { element }) {
+
+
+export default defineElement({
+    name: 'CustomElement',
+    components : {Explorer : Explorer},
+    setup(props, { element }) {
         const defaultClasses = ref({
-          container: '', // added automatically to the element's outermost DOM in ElementLayout
-          input: 'form-text-input',
-          input_danger: 'has-errors',
-          $input: (classes, { isDanger }) => ([
-            classes.input,
-            isDanger ? classes.input_danger : null,
-          ])
+            container: '', // added automatically to the element's outermost DOM in ElementLayout
+            input: 'form-text-input',
+            input_danger: 'has-errors',
+            $input: (classes, { isDanger }) => ([
+                classes.input,
+                isDanger ? classes.input_danger : null,
+            ])
         })
-  
+
         return {
-          defaultClasses,
+            defaultClasses,
         }
-      }
-    })
-  </script>
+    }
+})
+</script>
 
 
 <template>
     <ElementLayout>
-      <template #element>
-        <input
-          v-model="model"
-          :class="classes.input"
-        />
-      </template>
-  
-      <!-- Default element slots -->
-      <template v-for="(component, slot) in elementSlots" #[slot]><slot :name="slot" :el$="el$"><component :is="component" :el$="el$"/></slot></template>
+        <template #element>
+            <Explorer server="test"/>
+        </template>
+
+        <!-- Default element slots -->
+        <template v-for="(component, slot) in elementSlots" #[slot]>
+            <slot :name="slot" :el$="el$">
+                <component :is="component" :el$="el$" />
+            </slot>
+        </template>
     </ElementLayout>
-  </template>
-  
-  <style lang="scss">
-    .form-text-input {
-      border: 1px solid black;
-      outline: none;
-      width: 100%;
-  
-      &.has-errors {
+</template>
+
+<style lang="scss">
+.form-text-input {
+    border: 1px solid black;
+    outline: none;
+    width: 100%;
+
+    &.has-errors {
         border: 1px solid red;
-      }
     }
-  </style>
+}
+</style>
